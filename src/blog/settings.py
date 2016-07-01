c8f1e07142910a37f1f8f908b2e9ab933ab342d9
mendozadaniel <danielmendoza@Local-Network.local>
2016-07-01 15:05:13 -0700

    Added Postgres as the new database.

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -95,8 +95,9 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blog',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
